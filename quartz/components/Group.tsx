31147e83e93eb6f1ce5d953575cf2b416284ab29
Lia Howie <143102480+LiaHowie@users.noreply.github.com>
2025-12-03 10:50:00 +0000

    Update Group.tsx

--- quartz/components/Group.tsx
+++ quartz/components/Group.tsx
@@ -5,13 +5,14 @@ interface Options {
   children: any[]
 }
 
-const Group: QuartzComponent = ({ cfg, displayClass }: QuartzComponentProps & { opts: Options }) => {
-  const opts = cfg.opts as Options
+const Group: QuartzComponent = ({ cfg, displayClass }: QuartzComponentProps) => {
+  const title = cfg?.title
+  const children = cfg?.children ?? []
 
   return (
     <div class="bordered-wrapper">
-      {opts.title && <h3 class="group-title">{opts.title}</h3>}
-      {opts.children.map((Child, i) => (
+      {title && <h3 class="group-title">{title}</h3>}
+      {children.map((Child, i) => (
         <div key={i}>{Child}</div>
       ))}
     </div>
